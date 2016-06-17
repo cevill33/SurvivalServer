49c1f6602b8b85517533fcbeb4bbc7b013579352
mariusk <suriamk7@web.de>
2016-06-17 19:45:23 +0200

    Nick 2

--- src/me/survival/commands/Command_Nick.java
+++ src/me/survival/commands/Command_Nick.java
@@ -1,8 +1,11 @@
 package me.survival.commands;
 
+import me.survival.methods.NickNamer;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
@@ -15,8 +18,6 @@ public class Command_Nick implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player)sender;
         Inventory inv = p.getServer().createInventory(null,9,"§aNickNamer");
-
-
 
         p.openInventory(inv);
         return false;
