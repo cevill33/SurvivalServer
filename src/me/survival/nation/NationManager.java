2f480887e741d09d0904d4c7d51186ced4176332
cevill33 <vetox.team@gmail.com>
2016-06-30 19:50:37 +0200

    Verbesserung

--- src/me/survival/nation/NationManager.java
+++ src/me/survival/nation/NationManager.java
@@ -27,9 +27,9 @@ public class NationManager {
         }
         Inventory inv = p.getServer().createInventory(null,18,"§aNationManager");
 
-        inv.setItem(11,new ItemBuilder(Material.WOOL,1,(short)14).setDiplayname("§cAincrad").setLore(new String[]{"§7Trete der Aincraden Nation bei"}).build());
+        inv.setItem(11,new ItemBuilder(Material.WOOL,1,(short)14).setDiplayname(Nation.N1.getName()).setLore(new String[]{"§7Trete der " + Nation.N1.getName() + " Nation bei"}).build());
         inv.setItem(13,new ItemBuilder(Material.BARRIER).setDiplayname("§3Nationen").setLore(new String[]{"§7Trete einer Nation","§7bei um mit deiner Nation","§7gegen andere Nationen zu kämpfen!"}).build());
-        inv.setItem(15,new ItemBuilder(Material.WOOL,1,(short)11).setDiplayname("§2Trivia").setLore(new String[]{"§7Trete der Trivianischen Nation "}).build());
+        inv.setItem(15,new ItemBuilder(Material.WOOL,1,(short)11).setDiplayname(Nation.N2.getName()).setLore(new String[]{"§7Trete der " + Nation.N2.getName() + " Nation bei"}).build());
 
         p.openInventory(inv);
     }
